b169b7abb0417da481375ecfe126791bfd93f724
Leon Federico Huck <leonhuck007@gmail.com>
2025-02-13 17:20:34 -0300

    sumandole imagenes e iconos a la aplicacion

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,11 @@ import CreateViaje from './components/Viajes/CreateViaje';
 import ViajesPasajero from './components/Viajes/MisViajes';
 function App() {
   const items = [
-    {label: 'Viajes', icon:'pi pi-home', url:'/viajes'},
-    {label: 'Crear Usuario', icon:'pi pi-users', url:'/nuevo-usuario'},
-    {label: 'Login', icon:'pi pi-users', url:'/inicio-sesion'},
-    {label: 'Crear Viaje', icon:'pi pi-users', url:'/cargar-viaje'},
-    {label: 'Mis Viajes', icon:'pi pi-flower', url:'/mis-viajes'},
+    {label: 'Viajes', icon:'pi pi-car', url:'/viajes'},
+    {label: 'Crear Viaje', icon:'pi pi-warehouse', url:'/cargar-viaje'},
+    {label: 'Mis Viajes', icon:'pi pi-bookmark', url:'/mis-viajes'},
+    {label: 'Crear Usuario', icon:'pi pi-user', url:'/nuevo-usuario'},
+    {label: 'Login', icon:'pi pi-sign-in', url:'/inicio-sesion'},
     
   ]
   return (
